372f8202c853dd8f6e746e265405277e7dc1b5ee
quangnp1712 <quangnp1712@gmail.com>
2026-01-08 13:55:53 +0700

    sửa lại api endpoint cho web chạy api

--- lib/core/network/api/api_endpoints.dart
+++ lib/core/network/api/api_endpoints.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-// String domainUrl = String.fromEnvironment('DOMAIN',
-// defaultValue:
-//     'https://nps-core-service-6b8b355ec2a3.herokuapp.com' // Link dự phòng chạy local
-// );
-String domainUrl = "${dotenv.env["DOMAIN"]}";
+String domainUrl = String.fromEnvironment('DOMAIN',
+    defaultValue:
+        'https://nps-core-service-6b8b355ec2a3.herokuapp.com' // Link dự phòng chạy local
+    );
+// String domainUrl = "${dotenv.env["DOMAIN"]}";
 
 class ApiEndpoints {}
